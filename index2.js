76fc8ba981b63abc12cad29df522d8d9a1b38de0
Mihail <maikl6554452@gmsil.com>
2024-03-20 18:51:56 +0300

    *

--- index2.js
+++ index2.js
@@ -1,14 +1,14 @@
-let table;
+let table2;
 let currentCell = 0;
 
 function createTableIfNeeded() {
-	if (!table) {
-		table = document.createElement("table");
-		table.border = 1;
-		document.body.appendChild(table);
+	if (!table2) {
+		table2 = document.getElementById("table2");
+		table2.border = 1;
+		document.body.appendChild(table2);
 
 		for (let i = 0; i < 5; i++) {
-			const row = table.insertRow();
+			const row = table2.insertRow();
 			for (let j = 0; j < 6; j++) {
 				row.insertCell();
 			}
@@ -21,7 +21,7 @@ function foo() {
 
 	const randomNumber = Math.floor(Math.random() * 100);
 
-	const rows = table.rows;
+	const rows = table2.rows;
 	const totalCells = rows.length * rows[0].cells.length;
 	if (currentCell < totalCells) {
 		const rowIndex = Math.floor(currentCell / 6);
